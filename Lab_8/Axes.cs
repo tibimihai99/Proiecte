2bf67557c29c2adcf90ee373306e432f13f1333d
tibimihai99 <83176500+tibimihai99@users.noreply.github.com>
2021-12-22 20:26:07 +0200

    Add files via upload

---
+++ Lab_8/Axes.cs
@@ -0,0 +1,34 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+namespace Lab_8
+{
+    public class Axes
+    {
+        public Axes()
+        {
+
+        }
+
+        // desenare obiecte 3D 
+        //Desenează axe XYZ.
+        public void DeseneazaAxe()
+        {
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Red);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(75, 0, 0);
+            GL.End();
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Yellow);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 75, 0);
+            GL.End();
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Green);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 0, 75);
+            GL.End();
+        }
+    }
+}
---
+++ Lab_8/Cub3D.cs
@@ -0,0 +1,64 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+namespace Lab_8
+{
+    public class Cub3D
+    {
+        public Cub3D()
+        {
+
+        }
+
+        //Desenează cubul - quads.
+        public void DeseneazaCubQ(int nQuadsList, int[] arrQuadsList, int[,] arrVertex)
+        {
+            GL.Begin(PrimitiveType.Quads);
+            for (int i = 0; i < nQuadsList; i++)
+            {
+                switch (i % 4)
+                {
+                    case 0:
+                        GL.Color3(Color.Blue);
+                        break;
+                    case 1:
+                        GL.Color3(Color.Red);
+                        break;
+                    case 2:
+                        GL.Color3(Color.Green);
+                        break;
+                    case 3:
+                        GL.Color3(Color.Yellow);
+                        break;
+                }
+                int x = arrQuadsList[i];
+                GL.Vertex3(arrVertex[x, 0], arrVertex[x, 1], arrVertex[x, 2]);
+            }
+            GL.End();
+        }
+
+        //Desenează cubul - triunghuri.
+        public void DeseneazaCubT(int nTrianglesList, int[] arrTrianglesList, int[,] arrVertex)
+        {
+            GL.Begin(PrimitiveType.Triangles);
+            for (int i = 0; i < nTrianglesList; i++)
+            {
+                switch (i % 3)
+                {
+                    case 0:
+                        GL.Color3(Color.Blue);
+                        break;
+                    case 1:
+                        GL.Color3(Color.Red);
+                        break;
+                    case 2:
+                        GL.Color3(Color.Green);
+                        break;
+                }
+                int x = arrTrianglesList[i];
+                GL.Vertex3(arrVertex[x, 0], arrVertex[x, 1], arrVertex[x, 2]);
+            }
+            GL.End();
+        }
+    }
+}
